7065e97efb84124681b0d168b542fdbc3dd79799
GerardRey <g.rey36500@gmail.com>
2021-01-18 14:20:16 +0100

    Sistema de cantidad

--- panel-usuario/tienda/carrito/anadir_carrito.php
+++ panel-usuario/tienda/carrito/anadir_carrito.php
@@ -12,17 +12,42 @@
     } else {
         $datos = mysqli_fetch_assoc($res);
         $nombre_tabla = "carrito_".$_SESSION['nick'];
-        $resultado = mysqli_query($con, "insert into $nombre_tabla values(NULL,'$id',1,'$datos[nombre]','$datos[precio]')");
-
-        if(!$resultado) {
+        $resultado1 = mysqli_query($con, "select count(*) as cuantos from $nombre_tabla where id_personaje = '$id'");
+        $datos2 = mysqli_fetch_assoc($resultado1);
+        if(!$resultado1) {
             die("Error en introduccir los datos2");
         } else {
-            echo "<script type='text/JavaScript'>  
-			        alert('Se a realizado correctamente el Registro'); 
-				
-					window.location.replace('./../tienda.php')
-					</script>";
+            if($datos2['cuantos'] == 0) {
+
+                $resultado = mysqli_query($con, "insert into $nombre_tabla values(NULL,'$id',1,'$datos[nombre]','$datos[precio]')");
+    
+                if(!$resultado) {
+                    die("Error en introduccir los datos3");
+                } else {
+                    echo "<script type='text/JavaScript'>  
+                            alert('Se a añadido el articulo al Carrito'); 
+                        
+                            window.location.replace('./../tienda.php')
+                            </script>";
+                }
+    
+            } else {
+
+                $resultado = mysqli_query($con, "UPDATE $nombre_tabla SET cantidad = cantidad+1 WHERE id_personaje = $id");
+
+                if(!$resultado) {
+                    die("Error en introduccir los datos4");
+                } else {
+                    echo "<script type='text/JavaScript'>  
+                            alert('Se a añadido el articulo al Carrito'); 
+                        
+                            window.location.replace('./../tienda.php')
+                            </script>";
+                }
+    
+            }
         }
+        
     }
 
     
